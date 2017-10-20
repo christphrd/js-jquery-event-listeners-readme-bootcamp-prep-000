4edaa04c44574552b73ab358740d900aa7415596
Christopher Diep <diep.christopher@gmail.com>
2017-10-20 04:32:06 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,5 +1,29 @@
 //define functions here
 
+function getIt() {
+  $('p').on('click', function() {
+    alert("Hey!")
+  })
+}
+
+function frameIt() {
+  $('.tasty').on('load',function() {
+    
+  }) //load, red frame
+}
+
+function pressIt() {
+  $('#typing').on('keydown', function() {
+    if (key.which == 71){
+      alert('g was pressed');
+    }
+  }) //keydown G
+}
+
+function submitIt() {
+  // submit "Your form is going to be submitted now."
+}
+
 $(document).ready(function(){
 
 // call functions here
